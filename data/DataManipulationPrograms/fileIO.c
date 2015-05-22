5e468d6b63fbeb8865a42868a85ca91ad87ef29c
kumarsiddharth <kumar.siddharth450@gmail.com>
2015-05-22 16:23:18 +0530

    added raw reviews and filtered them too

--- data/DataManipulationPrograms/fileIO.c
+++ data/DataManipulationPrograms/fileIO.c
@@ -3,8 +3,8 @@
 
 char buffer[4096];
 static int readSenID = 0;
-static int newSenID = 134;
-char polrity[5];
+static int newSenID = 0;
+char polarity[5];
 int main(int argc, char **argv)
 {
     FILE *fptr, *fptr2;
@@ -19,13 +19,31 @@ int main(int argc, char **argv)
     fseek(fptr,0,SEEK_SET);
     
     
-    while((fscanf(fptr,"%d %s %s",&readSenID,polarity,buffer)) != NULL){
+  /*  while((fscanf(fptr,"%d %s %s",readSenID,polarity,buffer)) > 0){
        newSenID++;
        fprintf(stdout,"%d\t%s\t%s\n",newSenID,polarity,buffer);
        fprintf(fptr2,"%d\t%s\t%s",newSenID,polarity,buffer);
-    }
+    }*/
+    char dump[4096];
+    int offset;
+  while((fgets(buffer,sizeof(buffer),fptr)) != NULL){
+     offset = -(sizeof(buffer));
+     fseek(fptr, offset, SEEK_CUR);
+     fgetline(fptr);
+   
+  }
 
  fclose(fptr);
 
 return 0;
+}
+
+void fgetline(FILE *fptr)
+{
+    int l_senID, l_polarity;
+    fscanf(fptr,"%d %s",&l_senID, l_polarity);
+    fgets(buffer,sizeof(buffer),fptr);
+
+    printf("SentenceID: %d\n SentencePolarity: %s\n Text: %s\n",
+                            l_senID,l_polarity,buffer);
 }
